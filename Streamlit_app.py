b4a9b00e97e1937731263585d5b0a3206c369e2b
keverhans <keverhans@users.noreply.github.com>
2022-09-30 13:43:36 +0200

    Update Streamlit_app.py
    typo emoticon

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('\N{bowl with spoon}Omega 3 & Blueberry Oatmeal')
-streamlit.text('\N{salad}Kale, Spinach & Rocket Smoothie')
+streamlit.text('\N{green salad}Kale, Spinach & Rocket Smoothie')
 streamlit.text('\N{chicken}Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
