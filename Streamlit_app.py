e1bc85db515f4279bf717b8e6819ddea4a594ad6
keverhans <keverhans@users.noreply.github.com>
2022-09-30 13:39:06 +0200

    Update Streamlit_app.py
    emojis

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Favorites')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('\N{chicken}Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
