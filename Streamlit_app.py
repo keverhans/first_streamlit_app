260a0cd5b9b894aad2daad614f6545bd7375cffb
keverhans <keverhans@users.noreply.github.com>
2022-10-07 13:40:48 +0200

    Update Streamlit_app.py
    variable search

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -12,6 +12,8 @@ streamlit.text('\N{green salad}Kale, Spinach & Rocket Smoothie')
 streamlit.text('\N{chicken}Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
@@ -19,7 +21,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 streamlit.header("Fruityvice Fruit Advice!")
 # streamlit.text(fruityvice_response.json())
