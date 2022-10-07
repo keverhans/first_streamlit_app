89db29963df0fd0cd36196fe5257479b9d949b19
keverhans <keverhans@users.noreply.github.com>
2022-10-07 17:06:28 +0200

    Update Streamlit_app.py
    typo

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -48,7 +48,7 @@ try:
       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
       streamlit.dataframe(fruityvice_normalized)
-      streamlit.write('Thanks for adding ', add_my_fruit)
+      streamlit.write('Thanks for adding ', fruit_choose)
 except URLError as e:
   streamlit.error()
       
