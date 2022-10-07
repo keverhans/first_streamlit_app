9f9831904ca68a40db6c186dc0569ad97c91d526
keverhans <keverhans@users.noreply.github.com>
2022-10-07 17:04:58 +0200

    Update Streamlit_app.py
    url error

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
@@ -22,15 +23,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
 
 streamlit.header("Fruityvice Fruit Advice!")
 # streamlit.text(fruityvice_response.json())
 
-# make df
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# show table
-streamlit.dataframe(fruityvice_normalized)
+
 
 # snowflake test
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
@@ -41,5 +39,19 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 # allow the end user to add a fruit to the list
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
-streamlit.write('The user entered ', add_my_fruit)
+try:
+  add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+  if not fruit_choose:
+      streamlit.error("Please select a fruit to get information.")
+  else:
+      # make df
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalized)
+      streamlit.write('Thanks for adding ', add_my_fruit)
+except URLError as e:
+  streamlit.error()
+      
+
+
+
